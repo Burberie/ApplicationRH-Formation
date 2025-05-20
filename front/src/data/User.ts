71320e91a5eb11f0b7636eb7a16492d12c2dee7c
MarieRodiet <marie.rodiet@gmail.com>
2025-05-20 13:59:37 +0200

    git init front

---
+++ front/src/data/User.ts
@@ -0,0 +1,11 @@
+class User {
+	id?: number;
+	email?: string;
+	password?: string;
+	lastname?: string;
+	firstname?: string;
+	role?: string;
+}
+
+const person = new User();
+console.log(person);
